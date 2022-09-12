ff525f68b8428fe58ec87831a4c36dc6b9602bbe
costa <costantino.marullo@qlik.com>
2022-09-12 17:39:02 +0200

    feat: mashup example

---
+++ example/index.js
@@ -0,0 +1,67 @@
+const script = `
+Characters:
+Load Chr(RecNo()+Ord('A')-1) as Alpha, RecNo() as Num autogenerate 26;
+ASCII:
+Load
+ if(RecNo()>=65 and RecNo()<=90,RecNo()-64) as Num,
+ Chr(RecNo()) as AsciiAlpha,
+ RecNo() as AsciiNum
+autogenerate 255
+ Where (RecNo()>=32 and RecNo()<=126) or RecNo()>=160 ;
+Transactions:
+Load
+ TransLineID,
+ TransID,
+ mod(TransID,26)+1 as Num,
+ Pick(Ceil(3*Rand1),'A','B','C') as Dim1,
+ Pick(Ceil(6*Rand1),'a','b','c','d','e','f') as Dim2,
+ Pick(Ceil(3*Rand()),'X','Y','Z') as Dim3,
+ Round(1000*Rand()*Rand()*Rand1) as Expression1,
+ Round(  10*Rand()*Rand()*Rand1) as Expression2,
+ Round(Rand()*Rand1,0.00001) as Expression3;
+Load
+ Rand() as Rand1,
+ IterNo() as TransLineID,
+ RecNo() as TransID
+Autogenerate 1000
+ While Rand()<=0.5 or IterNo()=1;
+`;
+
+function connect() {
+    const loadSchema = () =>
+      fetch('https://unpkg.com/enigma.js/schemas/12.34.11.json').then((response) => response.json());
+    const createConnection = () =>
+      loadSchema().then((schema) =>
+        window.enigma
+          .create({
+            schema,
+            url: `ws://${window.location.hostname || 'localhost'}:9076/app/${Date.now()}`,
+          })
+          .open()
+          .then((qix) => qix.createSessionApp())
+      );
+
+    return createConnection().then((app) => app.setScript(script).then(() => app.doReload().then(() => app)));
+  }
+  
+  async function run() {
+    await connect().then((app) => {
+      const nuked = window.stardust.embed(app, {
+        context: { theme: 'light' },
+        types: [
+          {
+            name: 'filterpane',
+            load: () => Promise.resolve(window['sn-filter-pane']),
+          },
+        ],
+      });
+
+      nuked.render({
+        element: document.getElementById('object'),
+        type: 'filterpane',
+        fields: ['Alpha', '=Sum(Expression1)']
+      });
+    });
+  }
+  
+  run();
