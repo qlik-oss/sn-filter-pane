80290fee5d754a59def49b3e2bdb4d09144f36aa
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2022-09-13 13:55:39 +0200

    fix: cleanup

--- src/hooks/use-render.ts
+++ src/hooks/use-render.ts
@@ -1,7 +1,6 @@
 import { embed, stardust, useApp, useEffect, useElement, useLayout, useModel, useOptions, usePromise, useState } from "@nebula.js/stardust";
 import { store, IStore } from '../store';
 import getListBoxResources from "./listbox/get-listbox-resources";
-import renderListBox from "./listbox/render-listbox";
 import { IContainerElement, IListLayout, IListBoxOptions, IFilterPaneLayout, IListboxResource, ListboxResourcesArr } from './types';
 import { render, teardown } from "../components/root";
 import './style.scss';
@@ -14,6 +13,7 @@ interface IRenderArgs {
 
 interface IUseOptions {
   toggleExpand?: () => void; // Callback passed from hosting app. TODO: Set to onClick event on collapsed button
+  listboxOptions?: IListBoxOptions;
 }
 
 export default function useRender({ flags }: IRenderArgs ) {
@@ -29,7 +29,6 @@ export default function useRender({ flags }: IRenderArgs ) {
   store.setState({ app, model, layout });
 
   const containerElement = <IContainerElement>useElement();
-  const listboxOptions: IListBoxOptions = {}
 
   useEffect(() => {
     if (!app || !layout) {
@@ -44,59 +43,9 @@ export default function useRender({ flags }: IRenderArgs ) {
     if (!resourcesArr?.length || !app) {
       return;
     }
-    const root = render(containerElement, resourcesArr, app, listboxOptions);
+    const root = render(containerElement, resourcesArr, app, options.listboxOptions ?? {});
     return (() => {
       teardown(root);
     })
   }, [resourcesArr]);
-
-
-  // useEffect(() => {
-  //   if (!resourcesArr?.length) {
-  //     return;
-  //   }
-  // const lbInstances = resourcesArr.map((resources: IListboxResources, index: number) => {
-  //   const element = document.createElement('div');
-  //   element.id = `filterpane-container-${index}`;
-  //   element.className = 'filterpane-container';
-  //   element.style.height = '45%';
-  //   containerElement.appendChild(element);
-
-  //   // Assign an element container for each ListBox and render it within it.
-  //   return renderListBox({
-  //     resources: {
-  //       ...resources,
-  //       app,
-  //       isEnabled,
-  //     },
-  //     element,
-  //     options: {},
-  //   });
-  // });
-
-  // if (options.toggleExpand) { // TODO: Should only be visible when listboxes does not fit.
-  //   const button = document.createElement('button'); // TODO: Use lui-icon--more
-  //   button.innerHTML = 'expand...';
-  //   button.onclick = options.toggleExpand;
-  //   containerElement.appendChild(button);
-  // }
-
-  // return () => {
-  //   lbInstances.forEach(((destroy) => destroy()));
-  //   containerElement.replaceChildren();
-  // }
-// }, [resourcesArr]);
-
-  // [
-  //   // This dependency array should have props that must trigger a re-render
-  //   element,
-  //   fieldInstance,
-  //   selectionsApi,
-  //   search,
-  //   // Editing Data > Dimensions - Pane > Re-ordering panes
-  //   layout?.qInfo?.qId,
-  //   // Editing Data > Dimensions - Pane > Field
-  //   getFieldName(layout),
-  // ]);
-
 }
