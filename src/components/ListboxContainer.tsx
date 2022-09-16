43622c6612ef65372ea461bd3dcf3309bc07d794
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2022-09-16 10:43:09 +0200

    feat: try out masonry layout

--- src/components/ListboxContainer.tsx
+++ src/components/ListboxContainer.tsx
@@ -8,19 +8,23 @@ interface ListboxContainerProps {
   layout: IListLayout;
   app: EngineAPI.IApp;
   listboxOptions: IListBoxOptions;
+  containerHeight: number;
+  width: number;
 }
 
-const ListboxContainer = ({ layout, app, listboxOptions }: ListboxContainerProps) => {
+const ListboxContainer = ({ layout, app, listboxOptions, containerHeight, width }: ListboxContainerProps) => {
 
   const fieldName = getFieldName(layout);
   const [listboxInstance, setListboxInstance] = useState<stardust.FieldInstance>();
-  const elRef = useRef();
+  const elRef = useRef<HTMLElement>();
+  const [height, setHeight] = React.useState(300);
+  const [maxHeight, setMaxHeight] = React.useState(300);
 
   useEffect(() => {
     const nebbie = embed(app, {
-    //   context: {
-    //     language: translator.language,
-    //   },
+      //   context: {
+      //     language: translator.language,
+      //   },
     });
     nebbie.field(fieldName).then((inst: any) => setListboxInstance(inst));
   }, []);
@@ -29,22 +33,59 @@ const ListboxContainer = ({ layout, app, listboxOptions }: ListboxContainerProps
     if (!elRef.current || !listboxInstance) {
       return undefined;
     }
-
-    listboxInstance.mount(elRef.current, listboxOptions);
+    listboxInstance.mount(
+      elRef.current,
+      {
+        ...listboxOptions,
+        ...{
+          dense: false,
+        }
+      }
+    );
 
     return () => {
       listboxInstance.unmount();
     };
   }, [elRef.current, listboxInstance]);
 
+  useEffect(() => {
+    setHeight(containerHeight);
+  }, [containerHeight]);
+
+  useEffect(() => {
+    setMaxHeight(getBoxHeight(32))
+  }, []);
+
+  const getBoxHeight = (rowHeight: number) => {
+    const bottomPadding = 4;
+    const toolbarHeight = 48;
+    const searchBarHeight = 40;
+    const rowCount = layout.qListObject.qSize.qcy;
+    const maxHeightL = elRef.current?.parentElement?.getBoundingClientRect().height ?? 500;
+    // const maxHeight = 300;
+    let height = Math.min((rowHeight * rowCount) + bottomPadding + toolbarHeight + searchBarHeight, maxHeightL);
+    return height;
+    // return maxHeightL;
+  };
+
   return (
-    <Box
-      className="listbox-container"
-      sx={{
-        height: 400,
-      }}
-      ref={elRef}
-    />
+    <>
+      {console.log('rendering listbox')}
+      <Box
+        className="listbox-container"
+        sx={{
+          height: height,
+          maxHeight: maxHeight,
+          // maxHeight: getBoxHeight(32),
+          // width: width,
+          // flexGrow: 1,
+          // height: getBoxHeight(32),
+          // height: '100%',
+          border: '1px solid red',
+        }}
+        ref={elRef}
+      />
+    </>
   );
 };
 
--- src/components/ListboxGrid.tsx
+++ src/components/ListboxGrid.tsx
@@ -1,18 +1,96 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import { IListboxResource as IListboxResource } from '../hooks/types';
 import ListboxContainer from './ListboxContainer';
+import { ResizableBox } from 'react-resizable';
+import "react-resizable/css/styles.css";
+import ElementResizeListener from './ElementResizeListener';
+import { Box } from '@mui/system';
+import Masonry from '@mui/lab/Masonry';
+import debounce from "lodash/debounce"
 
 export default function ListboxGrid(props: any) {
   const { resources, app, listboxOptions } = props;
+  const [maxHeight, setMaxHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const gridRef = useRef<any>();
+
+  const handleResize = () => {
+    // debounce(() => {
+    setMaxHeight(gridRef.current?.offsetHeight);
+    setWidth((gridRef.current?.offsetWidth / resources.length) - 4);
+    // console.log({ settingNewHeight: maxHeight, width: width, oh: gridRef.current.offsetHeight, ow: gridRef.current.offsetWidth });
+    // }, 300)
+    // setMaxHeight('100%');
+    // setMaxHeight(gridRef.current?.getBoundingClientRect().height);
+    // setWidth((gridRef.current?.getBoundingClientRect().width / resources.length) - 4);
+
+  };
+
+  const dHandleResize = debounce(handleResize, 50);
+
+  useEffect(() => {
+    if (gridRef.current) {
+      console.log('init resize');
+      handleResize();
+    }
+  }, []);
+
+  // TODO: Remove ResizableBox, only for developing purposes
+  // return (
+  //   <>
+  //     <ResizableBox width={1100} height={1100} minConstraints={[100, 100]} maxConstraints={[1220, 1220]}>
+  //       <ElementResizeListener onResize={resizing} />
+  //       {/* <Box sx={{ flexGrow: 1 }}> */}
+  //         <Grid container className='filterpane-container' sx={{ border: '1px solid green' }} ref={gridRef} spacing={{ xs: 1 }}>
+  //           {resources.map((resource: IListboxResource, index: number) => (
+  //             <Grid item key={index} xs='auto'>
+  //               <ListboxContainer layout={resource.layout} app={app} listboxOptions={listboxOptions} maxHeight={maxHeight} width={width} />
+  //             </Grid>
+  //           ))}
+  //         </Grid>
+  //       {/* </Box> */}
+  //     </ResizableBox>
+  //   </>
+  // );
+
+  const getColumnCount = () => {
+    switch (true) {
+      case (width < 50):
+        console.log(1, width);
+        return 1;
+      case (width < 100):
+        console.log(2, width);
+        return 2;
+      case (width < 150):
+        console.log(3, width);
+        return 3;
+      default:
+        console.log(4, width);
+        return 4;
+    }
+  }
 
   return (
-    <Grid container className='filterpane-container'>
-      {resources.map((resource: IListboxResource, index: number) => (
-        <Grid item key={index}>
-          <ListboxContainer layout={resource.layout} app={app} listboxOptions={listboxOptions} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      {console.log('rendering box')}
+      <ResizableBox width={1100} height={1100} minConstraints={[100, 100]} maxConstraints={[1220, 1220]}>
+        <Box sx={{ width: '100%', height: '100%' }} ref={gridRef}>
+          <ElementResizeListener onResize={dHandleResize} />
+          <Masonry sx={{ border: '1px solid green' }} spacing={{ xs: 1 }} columns={getColumnCount()} ref={gridRef}>
+            {resources.map((resource: IListboxResource, index: number) => (
+              <ListboxContainer key={index} layout={resource.layout} app={app} listboxOptions={listboxOptions} containerHeight={maxHeight} width={width} />
+            ))}
+          </Masonry>
+        </Box>
+      </ResizableBox>
+    </>
   );
+
 }
+
+
+// Make a function that returns a number of how many listboxes that should be collapsed depending of the container size.
+
+
+
