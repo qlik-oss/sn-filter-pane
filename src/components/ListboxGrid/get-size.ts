0f6ca974c11b007fc860ca699ab02aa41195d467
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2022-09-21 13:03:39 +0200

    fix: limit max columns

--- src/components/ListboxGrid/get-size.ts
+++ src/components/ListboxGrid/get-size.ts
@@ -1,13 +1,16 @@
-export const getHeight = (refWidth: number, resourceCount: number) => {
-  const width = refWidth / resourceCount - 4;
+export const getWidth = (refWidth: number) => {
+  const width = refWidth - 4;
   return { newWidth: width };
 };
 
-export const getColumnCount = (width: number) => {
-  const maxColumnWidth = 50;
-  return Math.ceil(width / maxColumnWidth);
+export const getColumnCount = (width: number, maxColumns: number) => {
+  const maxColumnWidth = 170;
+  let count = Math.floor(width / maxColumnWidth);
+  count = Math.min(count, maxColumns);
+  count = Math.max(count, 1);
+  return count;
 };
 
-export const getColumnWidth = (width: number) => 100 / getColumnCount(width);
+export const getColumnWidthPercent = (width: number, maxColumns: number) => 100 / getColumnCount(width, maxColumns);
 
-export const getWidthForRestOfColumns = (width: number) => getColumnWidth(width) * (getColumnCount(width) - 1);
+export const getWidthForRestOfColumns = (width: number, maxColumns: number) => getColumnWidthPercent(width, maxColumns) * (getColumnCount(width, maxColumns) - 1);
