9934b798ccbcb77eb9da02362d767a57f91c3853
Johan Lahti <johanlahti@gmail.com>
2022-10-05 13:23:48 +0200

    refactor: get rid of duplicate filter pane handler

--- src/ext/index.ts
+++ src/ext/index.ts
@@ -1,28 +1,8 @@
 import pp from './property-panel';
-import FilterpaneHandler from './filterpane-handler';
-
-interface Config {
-  type: 'rows'|'columns'
-}
 
 export default function ext(env: object) {
-  const { translator } = env;
-
   return {
-    getCreatePropertyHandler(app) {
-      return new FilterpaneHandler({
-        app,
-        dimensionDefinition: {
-          min: 1,
-          max: 1000,
-          description(_: unknown, __: unknown, config: Config): string {
-            const translationProperty = config && config.type === 'rows' ? 'Visualizations.Descriptions.Row' : 'Visualizations.Descriptions.Column';
-            return translator.get(translationProperty);
-          },
-        },
-        translator,
-      });
-    },
+    usePropertyHandler: 'list-object-container-data',
     definition: <object>pp(env),
     support: {
       snapshot: true,
