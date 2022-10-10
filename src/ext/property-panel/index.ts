ea513c86dc0c92531721847a7e719c6b164e5799
Johan Lahti <johanlahti@users.noreply.github.com>
2022-10-10 12:57:14 +0200

    refactor: remove handler and instead add the usePropertyHandler property (#19)

--- src/ext/property-panel/index.ts
+++ src/ext/property-panel/index.ts
@@ -2,8 +2,9 @@ import sorting from './sorting';
 // import simpleSorting from "./simple-sorting";
 import settings from './settings';
 import getData from './data';
+import { IEnv } from '../../types/types';
 
-export default function pp(env) {
+export default function pp(env: IEnv) {
   const out = {
     type: 'items',
     component: 'accordion',
